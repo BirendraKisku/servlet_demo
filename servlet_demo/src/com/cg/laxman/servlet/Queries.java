359616fa9ed08da90239ccdac1ffdc8604e0031e
temp <temp>
2018-09-15 16:55:23 +0530

    removed unnecessary

--- servlet_demo/src/com/cg/laxman/servlet/Queries.java
+++
@@ -1,6 +0,0 @@
-package com.cg.laxman.servlet;
-
-public class Queries {
-	public static String check_login="select role from Users where login_id=? and password=?";
-	public static String view_programs="select * from Programs_Offered";
-}
